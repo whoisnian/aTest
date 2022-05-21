7a09bdb73b2fa195e363d1a1a5e233cbf47524af
whoisnian <zhuchangbao1998@gmail.com>
2022-05-22 03:06:40 +0800

    INIT: simplify android studio project

---
+++ app/src/main/java/com/whoisnian/atest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.whoisnian.atest;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ConstraintLayout layout_main = new ConstraintLayout(this);
+        TextView textView_hello = new TextView(layout_main.getContext());
+        textView_hello.setText("Hello World!");
+
+        LayoutParams layoutParams_hello = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        layoutParams_hello.bottomToBottom = ConstraintSet.PARENT_ID;
+        layoutParams_hello.endToEnd = ConstraintSet.PARENT_ID;
+        layoutParams_hello.startToStart = ConstraintSet.PARENT_ID;
+        layoutParams_hello.topToTop = ConstraintSet.PARENT_ID;
+
+        layout_main.addView(textView_hello, layoutParams_hello);
+        setContentView(layout_main);
+    }
+}
