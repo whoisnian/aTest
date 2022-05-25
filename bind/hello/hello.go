74639a86518918406013df1ecc1f318122d63cd3
whoisnian <zhuchangbao1998@gmail.com>
2022-05-26 01:32:00 +0800

    ADD: golang counter api

--- bind/hello/hello.go
+++ bind/hello/hello.go
@@ -1,5 +1,41 @@
 package hello
 
+import (
+	"io"
+	"net/http"
+	"strconv"
+)
+
 func Greetings(name string) string {
 	return "Hello, " + name + "!"
+}
+
+func GetCnt() int64 {
+	resp, err := http.Get("https://counter.whoisnian.workers.dev/cnt")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	return readAsInt(resp.Body)
+}
+
+func IncCnt() int64 {
+	resp, err := http.Get("https://counter.whoisnian.workers.dev/inc")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	return readAsInt(resp.Body)
+}
+
+func readAsInt(r io.Reader) int64 {
+	buf, err := io.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	res, err := strconv.ParseInt(string(buf), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return res
 }
