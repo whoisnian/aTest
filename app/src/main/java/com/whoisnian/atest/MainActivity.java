fbec993a1ac59d1ae074ce7fcd8a59f53638927d
whoisnian <zhuchangbao1998@gmail.com>
2022-06-03 17:03:33 +0800

    ADD: android button

--- app/src/main/java/com/whoisnian/atest/MainActivity.java
+++ app/src/main/java/com/whoisnian/atest/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView textView_hello;
     TextView textView_counter;
     Button button_counter;
+    Button button_date;
     AlertDialog.Builder dialogBuilder;
 
     @Override
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView_hello = new TextView(layout_main.getContext());
         textView_counter = new TextView(layout_main.getContext());
         button_counter = new Button(layout_main.getContext());
+        button_date = new Button(layout_main.getContext());
         dialogBuilder = new AlertDialog.Builder(layout_main.getContext());
 
         textView_hello.setId(generateViewId());
@@ -46,6 +48,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button_counter.setAllCaps(false);
         button_counter.setText("increase by one");
         button_counter.setOnClickListener(this);
+
+        button_date.setId(generateViewId());
+        button_date.setAllCaps(false);
+        button_date.setText("date");
+        button_date.setOnClickListener(this);
 
         LayoutParams lp_textView_hello = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         lp_textView_hello.topToTop = ConstraintSet.PARENT_ID;
@@ -68,36 +75,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lp_button_counter.rightToRight = ConstraintSet.PARENT_ID;
         lp_button_counter.verticalChainStyle = ConstraintSet.CHAIN_PACKED;
 
+        LayoutParams lp_button_date = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        lp_button_date.topToTop = ConstraintSet.PARENT_ID;
+        lp_button_date.bottomToBottom = ConstraintSet.PARENT_ID;
+        lp_button_date.leftToLeft = ConstraintSet.PARENT_ID;
+        lp_button_date.rightToRight = ConstraintSet.PARENT_ID;
+        lp_button_date.verticalBias = (float) 0.95;
+        lp_button_date.horizontalBias = (float) 0.9;
+
         layout_main.addView(textView_hello, lp_textView_hello);
         layout_main.addView(textView_counter, lp_textView_counter);
         layout_main.addView(button_counter, lp_button_counter);
+        layout_main.addView(button_date, lp_button_date);
         setContentView(layout_main);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        new Thread(() -> {
-            try {
-                String text = String.valueOf(Hello.getCnt());
-                layout_main.post(() -> textView_counter.setText(text));
-            } catch (Exception e) {
-                e.printStackTrace();
-                layout_main.post(() -> dialogBuilder.setTitle("⚠ Exception ⚠").setMessage(e.getMessage()).show());
-            }
-        }).start();
+        runAndCatchInNewThread(() -> {
+            String text = String.valueOf(Hello.getCnt());
+            runInUiThread(() -> textView_counter.setText(text));
+        });
     }
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == button_counter.getId()) {
+            runAndCatchInNewThread(() -> {
+                String text = String.valueOf(Hello.incCnt());
+                runInUiThread(() -> textView_counter.setText(text));
+            });
+        } else if (v.getId() == button_date.getId()) {
+            runAndCatchInNewThread(() -> {
+                String text = Hello.sshExec("date");
+                runInUiThread(() -> dialogBuilder.setTitle("� Result �").setMessage(text).show());
+            });
+        }
+    }
+
+    private void runInUiThread(Runnable action) {
+        layout_main.post(action);
+    }
+
+    private void runAndCatchInNewThread(RunnableWithException action) {
         new Thread(() -> {
             try {
-                String text = String.valueOf(Hello.incCnt());
-                layout_main.post(() -> textView_counter.setText(text));
+                action.run();
             } catch (Exception e) {
                 e.printStackTrace();
-                layout_main.post(() -> dialogBuilder.setTitle("⚠ Exception ⚠").setMessage(e.getMessage()).show());
+                runInUiThread(() -> dialogBuilder.setTitle("⚠ Exception ⚠").setMessage(e.getMessage()).show());
             }
         }).start();
+    }
+
+    @FunctionalInterface
+    public interface RunnableWithException {
+        void run() throws Exception;
     }
 }
